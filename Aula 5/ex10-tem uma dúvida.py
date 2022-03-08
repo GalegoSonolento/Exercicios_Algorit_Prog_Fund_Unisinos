e7038027e34a5d18eeda515473d4fda90a5be87b
HenryH <henriquehaag11@gmail.com>
2022-03-08 18:14:21 -0300

    Initial commit

---
+++ Aula 5/ex10-tem uma dúvida.py
@@ -0,0 +1,13 @@
+# Soma de valores de 0 até 198
+# Como fazer sem a soma de Gauss?
+
+print('Digite algo para iniciar a soma.')
+
+i = 0
+f = 1
+
+command = input('Iniciar? ')
+
+soma = 198*99
+
+print(soma)
